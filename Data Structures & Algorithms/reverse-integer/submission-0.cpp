889a69e2075f09e8093754fd0b29a9e1c9c1e7ab
Krishay Garg <76239938+krishaygarg@users.noreply.github.com>
2026-08-28 18:28:39 -0700

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int answer = 0;
+        bool negative = false;
+        if (x<0){
+            x = -x;
+            negative = true;
+        }
+        while (x>0){
+            if (answer>(INT_MAX-x%10)/10){
+                return 0;
+            }
+            answer = answer*10+x%10;
+            x=x/10;
+            cout << answer << endl;
+        }
+            
+        if (negative){
+            answer = -answer;
+        }
+        return answer;
+    }
+};
